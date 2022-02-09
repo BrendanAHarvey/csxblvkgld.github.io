195294396609d58ed9887dd6ffe738f4a1534bce
[Brendan Harvey] <[ba38@hotmail.co.uk]>
2022-02-09 16:49:41 +0000

    JS Fadein Animation added

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,8 @@
 
 import * as THREE from 'three';
 
+
+// Setup the scene, camera, and renderer.
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -10,32 +12,51 @@ const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
 });
 
+
+// Setup the screen and camera position.
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
+
+// Setup the 3D Shape.
 const geometry = new THREE.SphereGeometry(15, 32, 16);
 const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
 const sphere = new THREE.Mesh( geometry, material );
 scene.add( sphere );
 
+// Function to make the scene anaimation constantly loop.
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
 
+// Initialise function.
 animate()
 
 
 
+// Element Fade in and Out Effect
+function isVisible(element) {
+    let elementBox = element.getBoundingClientRect();
+    let distanceFromTop = -200;
 
-
-var head = document.querySelector("h1");
-
-function changeColor() {
-    head.style.color = "blue";
+    if(elementBox.top - window.innerHeight < distanceFromTop) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-head.addEventListener("click", changeColor);
+function scanDocument() {
+    let sectionList = document.querySelectorAll('...')
+    sectionList.forEach(function(section) {
+        if(isVisible(section)) {
+            section.classList.remove('hidden');
+        };
+    });
+}
+
+document.addEventListener("scroll", scanDocument);
