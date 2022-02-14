8b6d44bed44b1f176f749151a62f6ed5f097cd62
[Brendan Harvey] <[ba38@hotmail.co.uk]>
2022-02-14 15:10:22 +0000

    14/02/2022

--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,23 @@
-// import './portfolio/style.css'
-
-import * as THREE from 'three';
 
 
-// Setup the scene, camera, and renderer.
-const scene = new THREE.Scene();
+const aboutSection = document.querySelector(".about-section");
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const options = {
+    root: null,
+    threshold: 0,
+    rootMargin: "-150px"
+};
 
-const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#bg'),
-});
-
-
-// Setup the screen and camera position.
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-
-renderer.render(scene, camera);
-
-
-// Setup the 3D Shape.
-const geometry = new THREE.SphereGeometry(15, 32, 16);
-const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-const sphere = new THREE.Mesh( geometry, material );
-scene.add( sphere );
-
-// Function to make the scene anaimation constantly loop.
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-
-// Initialise function.
-animate()
-
-
-
-// Element Fade in and Out Effect
-function isVisible(element) {
-    let elementBox = element.getBoundingClientRect();
-    let distanceFromTop = -200;
-
-    if(elementBox.top - window.innerHeight < distanceFromTop) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function scanDocument() {
-    let sectionList = document.querySelectorAll('...')
-    sectionList.forEach(function(section) {
-        if(isVisible(section)) {
-            section.classList.remove('hidden');
-        };
+const appearOnScroll = new IntersectionObserver(function(enteries, appearOnScroll) {
+    enteries.forEach(entry => {
+        if(!entry.isIntersecting){
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
     });
-}
+}, options);
 
-document.addEventListener("scroll", scanDocument);
+appearOnScroll.observe(aboutSection);
+
